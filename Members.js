cd30f6a4dedb7fec52d70bb6f8039a946ca94d40
Sandeep <sandeeps4284@gmail.com>
2020-04-10 20:00:07 +0530

    first commit

---
+++ Members.js
@@ -0,0 +1,22 @@
+const members = [
+    {
+        id:1,
+        name:'john',
+        email:'john@company.com',
+        status:'active'
+    },
+    {
+        id:2,
+        name:'Vijay Prakash',
+        email:'vijay@company.com',
+        status:'active'
+    },
+    {
+        id:3,
+        name:'Ram',
+        email:'ram@company.com',
+        status:'active'
+    }
+]
+
+module.exports = members;
